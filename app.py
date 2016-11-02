5d1d9cbba48b1a28f0d3fad0231a668e966200fd
Kenneth Li <kennethli.3470@gmail.com>
2016-11-02 12:28:19 -0400

    Added some flask routes

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
 from flask import Flask, render_template, request, session, url_for, redirect
+from utils import misc, db
 
 app = Flask(__name__)
-app.secret_key = "KEY"
+app.secret_key = 'KEY'
 
-@app.route("/")
-def login():
+def isLoggedIn():
+  return 'username' in session
+
+@app.route('/')
+def default():
+  if isLoggedIn():
+    return redirect(url_for('stories'))
+  else:
+    return redirect(url_for('auth'))
+
+@app.route('/auth')
+def auth():
+  if isLoggedIn():
+    return redirect(url_for('default'))
+  else:
     return render_template('auth.html')
 
-#@app.route("/auth")
+@app.route('/login', methods = ['POST'])
+def login():
+  if 'username' in request.form and 'password' in request.form:
+    username = request.form['username']
+    password = request.form['password']
 
-#@app.route("/login")
+    if authUser(username, hash(password)):
+      session['username'] = username
 
-#@app.route("/register")
+  return redirect(url_for('default'))
 
-#@app.route("/logout")
+# @app.route('/register')
 
-#@app.route("/stories")
+# @app.route('/logout')
+
+# @app.route('/stories')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+  app.debug = True
+  app.run()
